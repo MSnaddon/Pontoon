4b47a0c4b6355d059dd6ea02a75f6e994470d8bd
Michael Snaddon <msnaddon0991@gmail.com>
2016-08-26 10:52:45 +0100

    Pairprog final commit

---
+++ specs/CardTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import cardGame.*;
+
+
+public class CardTest {
+
+  Card card;
+
+  @Before
+  public void before() {
+    card = new Card( Suit.CLUB, Value.QUEEN );
+  }
+
+  @Test
+  public void getSuit() {
+    assertEquals( Suit.CLUB, card.getSuit() );
+  }
+
+  @Test
+  public void getValue() {
+    assertEquals( Value.QUEEN, card.getValue() );
+  }
+
+}
---
+++ specs/PlayerTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import cardGame.*;
+
+public class PlayerTest {
+
+  Player player;
+  Card card;
+  Deck deck;
+
+  @Before
+  public void before() {
+    player = new Player( "Cardy McCardface");
+    deck = new Deck();
+  }
+
+  @Test
+  public void hasName() {
+    assertEquals( "Cardy McCardface", player.getName() );
+  }
+
+  @Test
+  public void startsWithNoCasrds() {
+    assertEquals( 0, player.cardCount() );
+  }
+
+  @Test
+  public void canGetCardFromDeck() {
+    player.getCardFromDeck(deck, 2);
+    assertEquals( 2, player.cardCount() );
+  }
+
+}
